8dbe7fac0074453db90d8746efc4311f9efea776
Orión Serrano <130587502+orionserrano115@users.noreply.github.com>
2026-04-13 23:25:59 +0000

    Dia uno terminado(sigue dia 2)

---
+++ dia.py
@@ -0,0 +1,29 @@
+#variables y tipo de variables
+'''
+nombre = input("Ingresa tu nombre: ");#texto
+edad = int(input("Ingresa tu edad: "));#entero
+altura = float(input("Ingresa tu altura"));#decimal
+activo = input("True or False: ")#booleano
+'''
+
+#opercaciones basicas 
+
+numero1 = 5
+numero2 = 5
+
+#suma
+
+suma = numero1 + numero2
+print(suma)
+
+resta = numero1 - numero2
+print(resta)
+
+multiplicacion = numero1 * numero2
+print(multiplicacion)
+
+division = numero1 / numero2
+print(division)
+
+#Dia 1 terminado
+#variables, tipos de datos opercaciones básicas
