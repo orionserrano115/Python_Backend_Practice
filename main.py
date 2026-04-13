905769ff3ca85ec2bb344ee19be2134242139c9a
Orión Serrano <130587502+orionserrano115@users.noreply.github.com>
2026-04-13 21:28:56 +0000

    primer diar de estudio(incompleto)

---
+++ main.py
@@ -0,0 +1,16 @@
+#Datos de usuario
+nombre = input("Ingresa tu nombre: ");
+edad = int(input("Ingresa tu edad: "));
+altura = float(input("Ingresa tu altura"));
+activo = input("True or False: ");
+
+lista = ["jugar","tipo de sangre", "origen etnico"];
+
+persona = {
+    nombre: "Orion",
+    edad : 29,
+    altura : 1.75,
+    lista : ["jugar video juegos", "musica", "ejercicio"]
+    
+}
+#Intentar corregir el error del diccionario
